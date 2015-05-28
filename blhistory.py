d1adbbede1e5e6d6bcfdaf2a08a3dbfafdd6857a
Pascal Guterman <pascal.guterman@dt.insu.cnrs.fr>
2015-05-28 14:16:33 +0200

    Solve firefox bug

--- blhistory.py
+++ blhistory.py
@@ -24,6 +24,8 @@ def get_local(origin):
     R = 6371 # Earth radius (km)
     iok = [i for i in range(0,cata.size) if r2[i]< (radius/(R*deg2rad))**2] # Tresh 
     
+    if len(iok) < 1: return []
+        
     # Extract catalog inside radius   
     d = np.sqrt(r2[iok])*R*deg2rad
     local= [dict(zip(x.dtype.names,x)) for x  in cata[iok]]
@@ -36,7 +38,8 @@ def get_local(origin):
     t0 = datetime.strptime(origin['date'],"%Y-%m-%dT%H:%M:%S.%fZ").replace(tzinfo=pytz.timezone('Europe/Paris')) # datetime structure
     tlocal =  [datetime.strptime(x['date'] + 'T' + x['time'],"%Y/%m/%dT%H:%M:%S.%f"). \
       replace(tzinfo=pytz.utc).astimezone(pytz.timezone('Europe/Paris')) for x in local]
-    #   Set 10 years extraction tmax,tmin
+      
+    #   Select 10 years extraction tmax,tmin
     if   t0 >= max(tlocal):     tmax = max(tlocal)  # Event more recent than catalog
     elif t0 > max(tlocal)-half: tmax= max(tlocal)   # Event close to catalog end  
     elif t0 > min(tlocal)+half: tmax=t0+half        # Event inside catalog 
@@ -44,7 +47,6 @@ def get_local(origin):
     else:                       tmax = t0+2*half    # Event older than catalog
     
     tmin = tmax - 2*half
-    print("tester tous les cas")
     local2 = [local[i] for i in range(0,len(local)) if tlocal[i] > tmin and tlocal[i] < tmax]
     
     return local2
@@ -86,14 +88,20 @@ def plotpolar(origin,local):
     frac_year.append(org_year)
     frac_hour.append(org_hour)
     dist.append(0.)
+    dist = np.asarray(dist)
     
     #  Graphic common frame
     from matplotlib import pyplot as plt
-    #tit = " Local History (SiHex) "
-    tit=2
+    tit = " Local Quakes  "
+    #tit=2
+    
+    natcol = '#663300' # natural, artif, plots background and frame colors
+    artfcol= '#0000FF'   
+    bgcol='AntiqueWhite'     
+    frmcol='LightGray'
     #tit = 'Local History (SiHex) of ' + origin['date'] + ' (utc) lon ' + str(origin['lon']) + ' lat ' + str(origin['lat'])
     if tit not in plt.get_figlabels():     # Create figure or make current
-        fig=plt.figure(num=tit, figsize=(14, 7), facecolor='none') # create
+        fig=plt.figure(num=tit,figsize=(14, 7), facecolor=frmcol) # create       
     else:
         fig=plt.figure(tit)                                # activate
         plt.clf()
@@ -104,14 +112,11 @@ def plotpolar(origin,local):
         print "blhistory : Nothing to plot"
         return
 
-    #plt.subplots_adjust(left=0.1, right=0.9, top=0.9, bottom=0.1)    
+    # Convert magnitude to surface area
     def Mwsurf(m):
         if not hasattr(m,'__iter__'): m = [m] # make singleton iterable
         return [10*(max(x,1.5)-.5)**2 for x in m]
 
-    natcol = '#663300'
-    artfcol= '#0000FF'
-        
         
     #%%  === Plot 1 : Radial Date ===   
     # Prepare data
@@ -123,18 +128,21 @@ def plotpolar(origin,local):
     theta = 2*np.pi*np.array(frac_hour)
     
     # Make axes
-    ax = plt.subplot(121, polar=True)        # Polar view
+    ax = plt.subplot(121, polar=True,axisbg=bgcol)        # Polar view
     ax.set_theta_zero_location("N")          # Clockwise origin up
     ax.set_theta_direction(-1)   
-    #plt.xlabel('Date radius', fontsize=12)
     
-    inner=[3000-x for x in frac_year]         # Substitute year with arbitrary descreasing index
+    inner=np.asarray([3000-x for x in frac_year])         # Substitute year with arbitrary descreasing index
     # Plot catalog and event
-    colors=[natcol if x['type'] in ('ke','se','ls') else artfcol for x in local] # red for natural, blue otherwise
-    area = Mwsurf([x['Mw'] for x in local])
+    natindex = [i for i in xrange(len(local)) if local[i]['type'] in ('ke','se','ls')] # Separate artificial from natural
+    artfindex = [i for i in xrange(len(local)) if local[i]['type'] not in ('ke','se','ls')] # to plot on top
+
+    area = Mwsurf([x['Mw'] for x in local]) # Convert magnitudes to spot surface
+    area = np.asarray(area)
     areaorg = Mwsurf(origin['m'])
-    plt.scatter(theta, inner, s=area, c=colors, alpha=.9, linewidth=0, zorder=2) # Plot catalog
-    plt.scatter(theta[-1], inner[-1],s=areaorg,facecolors='w',linewidth=3,edgecolors='r',zorder=3) # Overplot origin
+    plt.scatter(theta[natindex], inner[natindex], s=area[natindex], c=natcol, alpha=.9, linewidth=0, zorder=2) # Plot natural
+    plt.scatter(theta[artfindex], inner[artfindex], s=area[artfindex], c=artfcol, alpha=1, linewidth=0, zorder=3) # Plot artificial    
+    plt.scatter(theta[-1], inner[-1],s=areaorg,facecolors='w',linewidth=3,edgecolors='r',zorder=4) # Overplot origin
 
     #  Time angular axis
     labelsy =  ['Time-Date\n 0h', '','2h','','4h','','6h','','8h','','10h','','12h','','14h','','16h','','18h','','20h','','22h',''] # Subtitude labels
@@ -158,35 +166,36 @@ def plotpolar(origin,local):
     cntr = ax.axis()[2]      # aiguille
     plt.plot([0,theta[-1]],[cntr,inner[-1]],linewidth=1,c='k')   
     
-    #%%  === Plot 2 : Radial Distance ===  
-    
+    #%%  === Plot 2 : Radial Distance ===      
                                              # Substitute radial labels for descending years
     # Prepare legend
     ax2 = plt.subplot(122, polar=True)       # Polar view
-    aref = Mwsurf([1.5,2,3]) # Further legend of plot
-    p1=plt.scatter(theta[-1], dist[-1],s=aref[0],facecolors='k',edgecolors='k',linewidth=3)
-    p2=plt.scatter(theta[-1], dist[-1],s=aref[1],c=natcol,alpha=.9,linewidth=0)
-    p3=plt.scatter(theta[-1], dist[-1],s=aref[1],c=artfcol,alpha=.9,linewidth=0)
-    p4=plt.scatter(theta[-1], dist[-1],s=aref[0],facecolors='none',edgecolors='k',linewidth=1)
-    p5=plt.scatter(theta[-1], dist[-1],s=aref[1],facecolors='none',edgecolors='k',linewidth=1)
-    p6=plt.scatter(theta[-1], dist[-1],s=aref[2],facecolors='none',edgecolors='k',linewidth=1)
+    aref = Mwsurf([1.5,2,3]) # Prepare for legend
+    p1=plt.scatter(theta[-1], dist[-1],s=aref[0],facecolors='w',linewidth=1,edgecolors='r')    # Event
+    p2=plt.scatter(theta[-1], dist[-1],s=aref[1],c=natcol,alpha=.9,linewidth=0)                # Natural
+    p3=plt.scatter(theta[-1], dist[-1],s=aref[1],c=artfcol,alpha=.9,linewidth=0)               # Artificial
+    p4=plt.scatter(theta[-1], dist[-1],s=aref[0],facecolors='none',edgecolors='k',linewidth=1) # Mw < 1.5
+    p5=plt.scatter(theta[-1], dist[-1],s=aref[1],facecolors='none',edgecolors='k',linewidth=1) #  2
+    p6=plt.scatter(theta[-1], dist[-1],s=aref[2],facecolors='none',edgecolors='k',linewidth=1) #  3
     fig.delaxes(ax2)
 
     # Prepare polar parameters
-    ax2 = plt.subplot(122, polar=True)       
+    ax2 = plt.subplot(122, polar=True,axisbg=bgcol)       
     ax2.set_theta_zero_location("N")
     ax2.set_theta_direction(-1)
     innerrat = .10 # ratio of inner void disc
     
     inner = (0-innerrat*max(dist))/(1-innerrat)
     ax2.set_ylim(inner,max(dist))
+    
+    plt.scatter(theta[natindex], dist[natindex],s=area[natindex],c=natcol,alpha=.9,linewidth=0,zorder=2)
+    plt.scatter(theta[artfindex], dist[artfindex],s=area[artfindex],c=artfcol,alpha=1,linewidth=0,zorder=2)
+    plt.scatter(theta[-1], dist[-1],s=areaorg,facecolors='w',linewidth=3,edgecolors='r',zorder=4)
+    
+    legd = fig.legend((p1,p2,p3,p4,p5,p6),('Origin','Natural','Artificial','Mw <1.5','  2','  3'),'upper right',scatterpoints=1,prop={'size':10})
+    frm = legd.get_frame()
+    frm.set_facecolor(bgcol)
 
-    
-    # Plot
-    plt.scatter(theta, dist,s=area,c=colors,alpha=.9,linewidth=0)
-    plt.scatter(theta[-1], dist[-1],s=areaorg,facecolors='w',linewidth=3,edgecolors='r',zorder=3)
-    
-    fig.legend((p1,p2,p3,p4,p5,p6),('Origin','Natural','Artificial','Mw <1,5','  2','  3'),'upper right',scatterpoints=1,prop={'size':10})
     # Custom radial grid
     val=list(ax2.get_yticks()) 
     lab2 = ['%i' % x for x in val]
@@ -204,11 +213,13 @@ def plotpolar(origin,local):
     plt.draw()
     plt.show()
 
-    #plt.show()
-    
-#fig.savefig('circum.png')
 #%%    
 if __name__ == "__main__":
+    import os
+    print "PYTHONPATH " + os.environ['PYTHONPATH']
+    print "LD_LIBRARY_PATH " + os.environ['LD_LIBRARY_PATH']
+
+
     import bltools
     
     cfg = bltools.get_config()    
