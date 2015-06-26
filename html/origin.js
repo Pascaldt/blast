65172f285a2c413f14feb3f6c93da06cfe827eeb
Pascal Guterman <pascal.guterman@dt.insu.cnrs.fr>
2015-06-26 16:39:53 +0200

    minor change

--- html/origin.js
+++ html/origin.js
@@ -1,12 +1,12 @@
 // Event-dependent variables
-		var lat = 42.827789 
-		var lng = 1.808086 
-		var date = "2014-04-10T10:55:01.704308Z" 
-		var loctime = "12:55" 
-		var dlat = 26.988861 
-		var dlng = 27.566498 
-		var isnight= "Lunch" 
-		var weekday= "Thu" 
-		var idev= "" 
-		var idorg= "Origin#20140410105931.080028.5618" 
-		var m = 3.870929 
+		var lat = 45.143372 
+		var lng = 1.155580 
+		var date = "2015-05-12T15:40:44.843452Z" 
+		var loctime = "17:40" 
+		var dlat = 37.351145 
+		var dlng = 24.460811 
+		var isnight= "Jour" 
+		var weekday= "Tue" 
+		var idev= "gfz2015jgny" 
+		var idorg= "Origin#20150512171212.304419.4864" 
+		var m = 2.993958 
